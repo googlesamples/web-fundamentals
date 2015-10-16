1c4fe6992bf3e6a49acd2d95f7341636bcb63f7f
Pete LePage <pete.lepage@pobox.com>
2015-10-15 17:02:06 -0700

    Sync to current

---
+++ samples/resources/jekyll/example.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+export default class exampleClass {
+
+constructor () {
+    super();
+
+    console.log('Example Constructor');
+  }
+
+  exampleFunction () {
+    console.log('Example Function');
+  }
+
+}
